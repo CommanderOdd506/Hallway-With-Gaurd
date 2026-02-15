378af46936afcf43937b8238779688ed17d2b678
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-02-15 15:15:32 -0500

    implemented Character Controller

---
+++ HWG Project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public Transform head;
+    private float _pitch;
+    public float pitchMin = -80f;
+    public float pitchMax = 80f;
+
+    Vector2 _lookInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        _lookInput = value.Get<Vector2>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = _lookInput.x;
+        float mouseY = _lookInput.y;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        _pitch -= mouseY;
+        _pitch = Mathf.Clamp(_pitch, pitchMin, pitchMax);
+        head.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
+    }
+}
---
+++ HWG Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 _moveInput;
+    private CharacterController controller;
+    public float moveSpeed;
+    private bool jumpPressed;
+    public float jumpBuffer = 0.12f;
+    public float gravity = -30f;
+    public float groundStickForce = -5f;
+    public float jumpHeight = 1.1f;
+
+    private float _timeSinceJumpPressed = 0.13f;
+    private Vector3 _velocity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+        forward.Normalize();
+
+        Vector3 right = transform.right;
+        right.y = 0f;
+        right.Normalize();
+        bool isGrounded = controller.isGrounded;
+
+        _timeSinceJumpPressed += Time.deltaTime;
+
+        bool bufferedJump = _timeSinceJumpPressed <= jumpBuffer;
+
+        if (bufferedJump && isGrounded)
+        {
+            _timeSinceJumpPressed = jumpBuffer + 1f;
+            _velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+        }
+
+        if (isGrounded && _velocity.y < 0f) _velocity.y = groundStickForce;
+        else _velocity.y += gravity * Time.deltaTime;
+
+        Vector3 worldDirection = (forward * _moveInput.y + right  * _moveInput.x);
+        worldDirection = worldDirection.normalized;
+        Vector3 horizontal = worldDirection * moveSpeed;
+
+        Vector3 motion = horizontal + new Vector3(0f, _velocity.y, 0f);
+        controller.Move(motion * Time.deltaTime);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            _timeSinceJumpPressed = 0f;
+        }
+    }
+    void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+}
